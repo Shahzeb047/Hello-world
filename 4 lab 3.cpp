d279b8e66cf778a9825c842365dea46a9bf16e22
Shahzeb Ali <shahzebaliarain996@gmail.com>
2025-10-23 20:35:19 -0700

    Add files via upload

---
+++ 4 lab 3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int i=1;
+	int sum=0;
+	while(i<=10){
+	
+	sum+=i;
+	i++;
+	}
+	cout<<"the sum of first 10 natural numbers is:"<<sum;
+	return 0;
+
+	}
