ef0f629569f20fd9db4a59fdb5178b2dedb2d906
Shahzeb Ali <shahzebaliarain996@gmail.com>
2025-10-30 09:49:50 -0700

    Implement cube printing for numbers from 1 to N
    This program takes an integer input n and prints the cubes of numbers from 1 to n.

---
+++ C++/for loop 9.cpp
@@ -0,0 +1,11 @@
+// 9.Print cubes of numbers from 1–N.
+#include<iostream>
+using namespace std;
+int main (){
+	int n;
+	cout<<"enter the value of n";
+	cin>>n;
+	for(int i=1; i<=n; i++){
+		cout<<i*i*i<<endl;
+	}return 0;
+}
