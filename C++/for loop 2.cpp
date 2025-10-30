f5d776c7c4dc6d5cb83f5ad0aa3a23046bc7cd2c
Shahzeb Ali <shahzebaliarain996@gmail.com>
2025-10-30 09:44:07 -0700

    Implement countdown from N to 1
    This program prints numbers from N to 1 based on user input.

---
+++ C++/for loop 2.cpp
@@ -0,0 +1,13 @@
+//2.Print numbers from N to 1.
+#include<iostream>
+using namespace std;
+int main (){
+	int n;
+	cout<<"Enter the value of n";
+	cin>>n;
+	for(int i=n; i>=1; i--){
+	cout<<i<<endl;
+	}
+	
+	return 0;
+}
